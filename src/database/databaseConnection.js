248b1e99544e19c2519427b81b2a21ef1606f40c
Nabeel <muhammadnabeeljavede@gmail.com>
2025-03-26 01:05:05 +0500

    Update database connection logic to include database name in the connection URL.

--- src/database/databaseConnection.js
+++ src/database/databaseConnection.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
+import databaseName from "./databaseName.js";
 
 export const dbConnect = async () => {
     try {
-        const connection = await mongoose.connect(process.env.MONGODB_URL)
+        const connection = await mongoose.connect(`${process.env.MONGODB_URL}/${databaseName}`)
         console.log(`Connected to database: ${connection.connection.host}`)
     } catch (error) {
         console.log(`Error connecting to database: ${error.message}`)
