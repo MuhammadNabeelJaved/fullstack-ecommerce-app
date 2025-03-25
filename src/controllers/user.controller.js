52c6cbbb54f4ecacb84f61847cd81df7e7788276
Nabeel <muhammadnabeeljavede@gmail.com>
2025-03-26 00:55:48 +0500

    Refactor user registration logic to improve error handling and integrate Cloudinary for avatar uploads. The controller now checks for required fields and existing users before creating a new user.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,33 +2,39 @@ import asyncHandler from "../utils/asyncHandler.js"
 import { apiResponse } from "../utils/apiResponse.js"
 import { ApiError } from "../utils/apiErrors.js"
 import User from "../models/user.model.js"
+import Cloudinary from "../utils/Cloudinary.js"
 
 
 const register = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body
     const avatar = req.file ? req.file.path : null
 
-    if (!avatar) {
-        throw new ApiError(400, "Avatar is required")
+    try {
+        if (!name || !email || !password) {
+            throw new ApiError(400, "Please provide all fields your name, email and password")
+        }
+
+        const existingUser = await User.findOne({
+            $or: [
+                { email },
+                { name }
+            ]
+        })
+        if (existingUser) {
+            throw new ApiError(400, "User already exists")
+        }
+        if (avatar) {
+            const uploadedImage = await Cloudinary(avatar)
+            avatar = uploadedImage.secure_url
+        }
+
+        const user = await User.create({ name, email, password, avatar })
+        apiResponse(res, { data: user, message: "User registered successfully" })
+
+    } catch (error) {
+        throw new ApiError(500, error.message)
     }
 
-
-    if (!name || !email || !password) {
-        throw new ApiError(400, "Please provide all fields your name, email and password")
-    }
-
-    const existingUser = await User.findOne({
-        $or: [
-            { email },
-            { name }
-        ]
-    })
-    if (existingUser) {
-        throw new ApiError(400, "User already exists")
-    }
-
-    const user = await User.create({ name, email, password, avatar })
-    apiResponse(res, { data: user, message: "User registered successfully" })
 })
 
 
