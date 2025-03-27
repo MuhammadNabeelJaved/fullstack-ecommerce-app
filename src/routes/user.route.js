d61e0f75b14e9b93359efc619ca929c0eefd89c0
Nabeel <muhammadnabeeljavede@gmail.com>
2025-03-27 16:36:45 +0500

    Add refresh access token route to user routes
    - Updated user routes to include a new endpoint for refreshing access tokens.

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import upload from "../utils/multer.js"
-import { register, verifyEmail, login, logout, getCurrentUser, updateCurrentUserPassword, updateCurrentUserAvatar } from "../controllers/user.controller.js"
+import { register, verifyEmail, login, logout, getCurrentUser, updateCurrentUserPassword, updateCurrentUserAvatar, refreshAccessToken } from "../controllers/user.controller.js"
 import { isAuthenticated } from "../middlewares/auth.middleware.js"
 const router = Router()
 
@@ -11,5 +11,6 @@ router.route("/logout").post(isAuthenticated, logout)
 router.route("/current-user").get(isAuthenticated, getCurrentUser)
 router.route("/update-current-user").put(isAuthenticated, updateCurrentUserPassword)
 router.route("/update-current-user-avatar").put(isAuthenticated, upload.single("avatar"), updateCurrentUserAvatar)
+router.route("/refresh-access-token").post(isAuthenticated, refreshAccessToken)
 
 export default router
