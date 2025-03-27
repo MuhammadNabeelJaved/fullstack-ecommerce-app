be1c6c62bdcb8865bc9a1814a3d92a7667e123bb
Nabeel <muhammadnabeeljavede@gmail.com>
2025-03-28 00:44:47 +0500

    Add revies model.

---
+++ src/models/review.model.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const reviewSchema = new Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5
+    },
+    comment: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true })
+
+const Review = model("Review", reviewSchema)
+export default Review
