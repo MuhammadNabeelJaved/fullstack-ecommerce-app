83c193a47e398b756ddb571ed04acee0ed596ad7
Nabeel <muhammadnabeeljavede@gmail.com>
2025-04-03 22:29:10 +0500

    Remove express dependency and clean up package-lock.json. Add PayPal SDK configuration in a new paypal.js file.

---
+++ src/paypal.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import paypal from "@paypal/paypal-server-sdk";
+
+dotenv.config();
+
+paypal.configure({
+    mode: "sandbox",
+    clientId: process.env.PAYPAL_CLIENT_ID,
+    clientSecret: process.env.PAYPAL_CLIENT_SECRET,
+});
+
+export default paypal;
+
